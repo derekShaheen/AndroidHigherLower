d4bb5732e7df270a4ca67906d3dc905dff384b5f
Derek Shaheen <dshaheen@css.edu>
2020-04-19 23:44:23 -0500

    Completed the strings.xml file. Development for Sprint 2 is underway.

--- app/src/main/java/com/shaheen/higherlower/MainActivity.java
+++ app/src/main/java/com/shaheen/higherlower/MainActivity.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * @author Derek Shaheen
@@ -19,13 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton buttonHigher;
     ImageButton buttonLower;
+    TextView tvCurrentNumber;
+    TextView tvCurrentScore;
+    TextView tvCurrentRound;
+    TextView tvHighScore;
+    TextView tvIndicator;
+
+    private int choiceResult = -99;
+
+    HigherLowerModel theModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        theModel = new HigherLowerModel();
+
         setupCallButtonClickEvents();
+
+        tvCurrentNumber = (TextView) findViewById(R.id.tvCurrentNumber);
+        tvCurrentRound = (TextView) findViewById(R.id.tvRoundNumber);
+        tvCurrentScore = (TextView) findViewById(R.id.tvCurrentScore);
+        tvIndicator = (TextView) findViewById(R.id.tvIndicator);
+
+        tvCurrentNumber.setText(theModel.getString());
     }
 
     private void setupCallButtonClickEvents() {
@@ -34,7 +55,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //
-                Log.d("out", "Working great!");
+                theModel.gen();
+                choiceResult = theModel.checkChoice(1);
+                setTextboxes();
             }
         });
 
@@ -43,9 +66,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //
-                Log.d("out", "Looking good, yo!");
+                theModel.gen();
+                choiceResult = theModel.checkChoice(0);
+                setTextboxes();
             }
         });
+    }
+
+    private void setTextboxes(){
+        if(choiceResult > 0) {
+            tvIndicator.setText("Correct!");
+            tvCurrentScore.setTextColor(getResources().getColor(R.color.gameCorrect));
+        }
+        else {
+            tvIndicator.setText("Incorrect!");
+            tvCurrentScore.setTextColor(getResources().getColor(R.color.gameIncorrect));
+        }
+        tvCurrentScore.setText(theModel.getScore());
+        tvCurrentNumber.setText(theModel.getString());
+        tvCurrentRound.setText("");
     }
 
 
